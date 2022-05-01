44d88f95444fcaa4ba172d0b1314c77f4a3647ed
andreaskastl <22062394+andreaskastl@users.noreply.github.com>
2022-05-01 17:50:09 +0200

    composer.json fix

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,7 +18,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '3.2.0',
     'constraints' => [
         'depends' => [
-            'php' => '7.4.0-8.1.99',        
+            'php' => '7.4.0-8.1.99',
             'typo3' => '10.4.20-11.5.99'
         ],
         'conflicts' => [],
