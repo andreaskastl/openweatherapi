2d6d6c29e3bc2618fa29553e466102d991e3ebad
andreaskastl <22062394+andreaskastl@users.noreply.github.com>
2025-04-21 14:28:31 +0200

    Code cleanup

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -3,22 +3,17 @@ declare(strict_types=1);
 
 defined('TYPO3') or die();
 
-(static function (): void {
+// register plugin
+$pluginSignature = \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'openweatherapi',
+    'weather',
+    'Openweather API - Weather Forecast',
+    'openweatherapi-weather'
+);
 
-    // register plugin
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-        'openweatherapi',
-        'weather',
-        'Openweather API - Weather Forecast',
-        'openweatherapi-weather'
-    );
-
-    // add flexform
-    $pluginSignature = 'openweatherapi_weather';
-    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-        $pluginSignature,
-        'FILE:EXT:openweatherapi/Configuration/FlexForms/Registration.xml'
-    );
-
-})();
+// add flexform
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    $pluginSignature,
+    'FILE:EXT:openweatherapi/Configuration/FlexForms/Registration.xml'
+);
